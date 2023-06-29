c9e66a9ddcde50f5db192dca6e6747d3c5263a50
kalpeshsc <kalpeshsc@teamlocus.ent>
2023-06-29 19:17:45 +0530

    screenshot utility added

--- loanbookforsale/src/main/java/loanbookforsale/qa/util/TestUtil.java
+++ loanbookforsale/src/main/java/loanbookforsale/qa/util/TestUtil.java
@@ -1,5 +1,29 @@
 package loanbookforsale.qa.util;
 
-public class TestUtil {
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import loanbookforsale.qa.base.TestBase;
+
+public class TestUtil extends TestBase{
+
+
+public  void takeScreenshotAtEndOfTestCase(){
+File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String currentDir = System.getProperty("user.dir");
+        // System.out.println(currentDir);
+        File path= new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png");
+		try{
+        FileUtils.copyFile(scrFile, path);
+        }
+        catch(IOException e){
+                System.out.println(e.getMessage());
+        }
+    }
+   
 
 }
