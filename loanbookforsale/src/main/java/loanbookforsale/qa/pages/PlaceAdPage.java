9b82827f1b5f857ee1d1c6c672573446cb5e3f54
chavan-kalpesh-Automation <chavankalpesh009@gmail.com>
2023-07-02 13:35:44 +0530

    place ad commit

---
+++ loanbookforsale/src/main/java/loanbookforsale/qa/pages/PlaceAdPage.java
@@ -0,0 +1,45 @@
+package loanbookforsale.qa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import loanbookforsale.qa.base.TestBase;
+
+public class PlaceAdPage extends TestBase {
+
+//PageFactory
+
+@FindBy(xpath = "//input[@name='name']")
+	WebElement titlefield;
+
+@FindBy(xpath = "//input[@name='name']")
+	WebElement contentield;
+
+@FindBy(xpath = "//input[@name='name']")
+	WebElement imagefield;
+
+@FindBy(xpath = "//input[@name='name']")
+	WebElement aggregatorfield;
+
+@FindBy(xpath = "//input[@name='name']")
+	WebElement categoryfield; 
+
+@FindBy(xpath = "//input[@name='name']")
+	WebElement locationfield;  
+ 
+@FindBy(xpath = "//input[@name='name']")
+	WebElement latifield; 
+
+@FindBy(xpath = "//input[@name='name']")
+	WebElement longifield; 
+    
+@FindBy(xpath = "//input[@name='name']")
+	WebElement pricefield;  
+
+@FindBy(xpath = "//input[@name='name']")
+	WebElement loanbooksizefield; 
+    
+@FindBy(xpath = "//input[@name='name']")
+	WebElement savebtn;  
+
+}
---
+++ loanbookforsale/src/test/PlaceAdPageTest.java
@@ -0,0 +1,7 @@
+package loanbookforsale.qa.test;
+
+import loanbookforsale.qa.base.TestBase;
+
+public class PlaceAdPageTest extends TestBase {
+
+}
