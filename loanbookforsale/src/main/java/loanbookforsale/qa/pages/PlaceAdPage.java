b0628c0d33a3e086b65a67a1a6dcab90e6a9b69f
kalpeshsc <kalpeshsc@teamlocus.ent>
2023-07-04 10:24:08 +0530

    placeas page code update

--- loanbookforsale/src/main/java/loanbookforsale/qa/pages/PlaceAdPage.java
+++ loanbookforsale/src/main/java/loanbookforsale/qa/pages/PlaceAdPage.java
@@ -2,6 +2,7 @@ package loanbookforsale.qa.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import loanbookforsale.qa.base.TestBase;
 
@@ -41,5 +42,17 @@ public class PlaceAdPage extends TestBase {
     
 @FindBy(xpath = "//input[@name='name']")
 	WebElement savebtn;  
+
+public PlaceAdPage(){
+	PageFactory.initElements(driver,this);
+
+}
+//Page Actions
+public void verifyTitleofPage(){
+	
+}
+
+
+
 
 }
--- loanbookforsale/src/test/java/loanbookforsale/qa/test/PlaceAdPageTest.java
+++ loanbookforsale/src/test/java/loanbookforsale/qa/test/PlaceAdPageTest.java
@@ -1,8 +1,28 @@
 package loanbookforsale.qa.test;
 
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 import loanbookforsale.qa.base.TestBase;
+import loanbookforsale.qa.pages.PlaceAdPage;
 
 public class PlaceAdPageTest extends TestBase {
+PlaceAdPage placeadpage ;
+
+public PlaceAdPageTest(){
+    super();
+
+}
+@BeforeMethod
+public void setup(){
+    initialization();
+    placeadpage = new  PlaceAdPage(); 
+}
+
+@Test
+public void verifyPlaceAdPageTitle(){
+
+}
 
 }
