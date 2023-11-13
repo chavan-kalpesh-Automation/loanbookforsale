211907d2ae2314e2a69af5a503172c6493cfa441
chavan-kalpesh-Automation <chavankalpesh009@gmail.com>
2023-11-13 12:56:48 +0530

    Contact us page test update

--- loanbookforsale/src/main/java/loanbookforsale/qa/pages/ContactUsPage.java
+++ loanbookforsale/src/main/java/loanbookforsale/qa/pages/ContactUsPage.java
@@ -6,46 +6,51 @@ import org.openqa.selenium.support.PageFactory;
 
 import loanbookforsale.qa.base.TestBase;
 
-public class ContactUsPage extends TestBase{
-    @FindBy (xpath="")
-    WebElement contactustxt;
+public class ContactUsPage extends TestBase {
+	@FindBy(xpath = "")
+	WebElement contactustxt;
 
-    @FindBy (xpath="//input[@name='name']")
-    WebElement namefield;
+	@FindBy(xpath = "//input[@name='name']")
+	WebElement namefield;
 
-    @FindBy (xpath="//input[@name='email']")
-    WebElement emailfield;
+	@FindBy(xpath = "//input[@name='email']")
+	WebElement emailfield;
 
-    @FindBy (xpath="//input[@name='subject']")
-    WebElement subjectfield;
+	@FindBy(xpath = "//input[@name='subject']")
+	WebElement subjectfield;
 
-    @FindBy (xpath="//input[@name='phone']")
-    WebElement phonenumberfield;
+	@FindBy(xpath = "//input[@name='phone']")
+	WebElement phonenumberfield;
 
-    @FindBy(xpath="//textarea[@name='content']")
-    WebElement messagefield;
+	@FindBy(xpath = "//textarea[@name='content']")
+	WebElement messagefield;
 
-    @FindBy (xpath="//div[@class='form-actions form-group']")
-    WebElement sendmsgbtn;
-    
+	@FindBy(xpath = "//div[@class='form-actions form-group']")
+	WebElement sendmsgbtn;
 
+	@FindBy(xpath = "//div[@class='contact-info']//div[text()=' info@loanbookforsale.com.au ']")
+	WebElement emailtext;
+
+	
+	
 //Initializing Page Object 
-    public ContactUsPage(){
-        PageFactory.initElements(driver,this);
-    }    
+	public ContactUsPage() {
+		PageFactory.initElements(driver, this);
+	}
 
 //Page Actions
-    public String  validateContactUsPageTitle(){
-        return driver.getTitle();
-    }
-    public void validateContactUsForm(String name,String email,String subject,String phone,String message){
-        namefield.sendKeys(name);
-        emailfield.sendKeys(email);
-        subjectfield.sendKeys(subject);
-        phonenumberfield.sendKeys(phone);
-        messagefield.sendKeys(message);
-        //Comment this click due to script run on live project 
-        // sendmsgbtn.click();   
+	public String validateContactUsPageTitle() {
+		return driver.getTitle();
+	}
 
-    }
+	public void validateContactUsForm(String name, String email, String subject, String phone, String message) {
+		namefield.sendKeys(name);
+		emailfield.sendKeys(email);
+		subjectfield.sendKeys(subject);
+		phonenumberfield.sendKeys(phone);
+		messagefield.sendKeys(message);
+		// Comment this click due to script run on live project
+		// sendmsgbtn.click();
+
+	}
 }
--- loanbookforsale/src/test/java/loanbookforsale/qa/test/ContactUsPageTest.java
+++ loanbookforsale/src/test/java/loanbookforsale/qa/test/ContactUsPageTest.java
@@ -1,5 +1,40 @@
 package loanbookforsale.qa.test;
 
-public class ContactUsPageTest {
-    
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import loanbookforsale.qa.base.TestBase;
+import loanbookforsale.qa.pages.ContactUsPage;
+import loanbookforsale.qa.pages.HomePage;
+
+public class ContactUsPageTest extends TestBase {
+	ContactUsPage contactpg;
+	HomePage homepg;
+
+	public ContactUsPageTest() {
+		super();
+	}
+
+	@BeforeMethod
+	public void setup() {
+		initialization();
+		contactpg = new ContactUsPage();
+		homepg = new HomePage();
+		contactpg = homepg.clickOnContactUsPage();
+
+	}
+
+	@Test
+	public void contactUsTitleTest() {
+		String title = contactpg.validateContactUsPageTitle();
+		Assert.assertEquals("Trail Books and Loan Books For Sale", title,
+				"Title of ContactUsPage Not Matched to Actual Title");
+		System.out.println(title);
+	}
+	@Test
+	public void contactUsFormTest() {
+		contactpg.validateContactUsForm(prop.getProperty("name"), prop.getProperty("email"), prop.getProperty("subject"),prop.getProperty("Phone"),prop.getProperty("Message"));
+	}
+
 }
